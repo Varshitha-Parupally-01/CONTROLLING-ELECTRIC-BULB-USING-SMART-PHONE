48d60592161a9ea950797f6b073da222a5522ed0
Parupally Varshitha <varshitha.parupally01@gmail.com>
2025-08-26 15:02:16 +0530

    code change

---
+++ code.c
@@ -0,0 +1,27 @@
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+/* Fill in information from Blynk Device Info here */
+//#define BLYNK_TEMPLATE_ID "TMPxxxxxx"
+//#define BLYNK_TEMPLATE_NAME "Device"
+//#define BLYNK_AUTH_TOKEN "YourAuthToken"
+#define BLYNK_TEMPLATE_ID "TMPL3p6HfzU2r"
+#define BLYNK_TEMPLATE_NAME "Electric Bulb"
+#define BLYNK_AUTH_TOKEN "G8a56L7wSDXIbN9xFdlOjDNJElwW8VYQ"
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "varshitha";
+char pass[] = "parupally_01";
+void setup()
+{
+// Debug console
+Serial.begin(9600);
+Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+}
+void loop()
+{
+Blynk.run();
+}
